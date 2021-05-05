10e88186b5096413aa3a265b091c71aa9d7761b1
sdl60660 <learnersd@gmail.com>
2021-05-05 15:05:07 -0400

    Added data from new 2021 crawl

--- data_processing/process_photos.py
+++ data_processing/process_photos.py
@@ -7,10 +7,10 @@ import time
 
 
 start = time.time()
-counter = iter(range(0, 40000))
+counter = iter(range(0, 100000))
 
-def get_main_color(img_path):
-	true_path = './data/' + img_path
+def get_main_color(img_path, parent_directory):
+	true_path = f'./{parent_directory}/' + img_path
 	color_thief = ColorThief(true_path)
 	dominant_color = color_thief.get_color(quality=1)
 
@@ -18,12 +18,15 @@ def get_main_color(img_path):
 
 	return dominant_color
 
+def main(parent_directory):
+	cover_data = pd.read_csv(f'{parent_directory}/main_dataset.csv')
 
-cover_data = pd.read_csv('data/main_dataset.csv')
+	cover_data['main_cover_color_rgb'] = cover_data['img_paths'].apply(lambda img_path: get_main_color(img_path, parent_directory))
+	cover_data['main_cover_color_hex'] = cover_data['main_cover_color_rgb'].apply(lambda x: rgb2hex(*x))
+	cover_data['main_cover_color_hsv'] = cover_data['main_cover_color_rgb'].apply(lambda x: rgb2hsv(*x, normalised=False))
+	cover_data['main_cover_color_lab'] = cover_data['main_cover_color_rgb'].apply(lambda x: color.rgb2lab([i / 255 for i in x]))
 
-cover_data['main_cover_color_rgb'] = cover_data['img_paths'].apply(lambda img_path: get_main_color(img_path))
-cover_data['main_cover_color_hex'] = cover_data['main_cover_color_rgb'].apply(lambda x: rgb2hex(*x))
-cover_data['main_cover_color_hsv'] = cover_data['main_cover_color_rgb'].apply(lambda x: rgb2hsv(*x, normalised=False))
-cover_data['main_cover_color_lab'] = cover_data['main_cover_color_rgb'].apply(lambda x: color.rgb2lab([i / 255 for i in x]))
+	cover_data.to_csv(f'./{parent_directory}/main_dataset_processed_colors.csv', index=False)
 
-cover_data.to_csv('./data/main_dataset_processed_colors.csv', index=False)
+if __name__ == "__main__":
+	main("updated_data")
